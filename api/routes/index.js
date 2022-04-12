d6b1c4c21ac00e7b25b96cd568a9014dda4f6d2c
Jared Hammond <jared@jaredhammond.me>
2022-04-12 08:19:56 -0400

    Added api .env to gitignore (oops) API Keys changed

--- api/routes/index.js
+++ api/routes/index.js
@@ -4,16 +4,24 @@ const authController = require("../controllers/authController");
 // const messageController = require("../controllers/messageController");
 // const profileController = require("../controllers/profileController");
 
-/* GET home page. */
-router.get("/", function (req, res, next) {
-  res.redirect("/messages");
-});
 
+//////////////////////////
 /* Authorization Routes */
+//////////////////////////
+
+// POST to create new user object
 router.post("/user", authController.user_post);
 
+// POST to login user and get JSON webtoken
 // router.post("/login", authController.login_post);
 
+// POST to logout user and remove JWT from the list of active JWTs
 // router.post("/logout", authController.logout_post);
+
+
+////////////////////
+/* Message routes */
+////////////////////
+router.get("/messages", );
 
 module.exports = router;
