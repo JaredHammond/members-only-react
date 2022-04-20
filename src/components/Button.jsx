6cded484cd7e1f59df64b5e31c37c5aeca936042
Jared Hammond <jared@jaredhammond.me>
2022-04-20 16:48:43 -0400

    WIP Button styled component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,10 +1,31 @@
+import styled from "styled-components";
+
 const Button = (props) => {
-  const { onClick, text, type } = props;
+  const { onClick, text, type, big } = props;
+
+  const StyledButton = styled.button`
+    width: ${big ? "100%" : "fit-content"};
+    height: ${big ? "4rem" : "2.5rem"};
+    max-width: ${big ? "500px" : "fit-content"};
+
+    background: #5e4ae3;
+    border-radius: 4px;
+    border: 0.2rem solid #5e4ae3;
+    color: #fff;
+    font-size: 1.6rem;
+    font-weight: bold;
+
+
+    &:hover {
+      background: #fff;
+      color: #5e4ae3;
+    }
+  `
 
   return (
-    <button onClick={onClick} className="btn small-btn" type={type || "button"}>
+    <StyledButton onClick={onClick} type={type || "button"}>
       {text}
-    </button>
+    </StyledButton>
   );
 };
 
